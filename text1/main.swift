34a38bccb9a1dc4fffe6769c123a643c4d71a39d
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-09-06 08:56:30 +0800

    Initial Commit

---
+++ text1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  text1
+//
+//  Created by s20181102929 on 2019/9/6.
+//  Copyright © 2019 s20181102929. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
